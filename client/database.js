2c9d9336fd38fb6cfbcd026a2db1b1277e25b55b
jaffer <eriklab1222@yahoo.com>
2016-07-27 23:00:50 -0700

    added a bit more comments

--- client/database.js
+++ client/database.js
@@ -1,10 +1,11 @@
 //var db = require('../model/index.js');
 //var models = require("../model");
-var options = {};
+var options = {}; //options are empty
 var pgp = require('pg-promise')(options);
-var db = pgp('postgress://userN:p_word@localhost:5432/mydb');
+var db = pgp('postgress://UserN:p_word@localhost:5432/mydb');
 
-
+//Function that adds a user to the postgres database
+//uses sql query language instead of sequelize now
 addUser = function(firstname, lastname, email, username, pass, done ) {
 	var date = today(); 
 	var query = "INSERT INTO users" + "(firstname, lastname, email, username, pass, reg_date)" +
@@ -26,7 +27,9 @@ addUser = function(firstname, lastname, email, username, pass, done ) {
 		done(null, false);
 	})  
 }
-	
+
+/*function to get the current day so that it can be used for 
+the registration date field */	
 function today() {
 		var d = new Date();
 		var n = d.getDate();
